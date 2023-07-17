52680222d26798a0bfc04c69abb2df974818317a
12ajayvishwa <ajayvish21099@gmail.com>
2023-07-17 12:15:17 +0530

    update

--- db/config.js
+++ db/config.js
@@ -1,2 +1,8 @@
+// const { default: mongoose } = require('mongoose');
 const mongoose = require('mongoose');
-mongoose.connect("mongodb://0.0.0.0:27017/e-commerce");
+mongoose.connect("mongodb+srv://ajayvish21099:ajayvish21099@cluster0.sdpqsau.mongodb.net/e-commerce?retryWrites=true&w=majority",{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+}).then(() => {
+        console.log(`connection successful`)
+    }).catch((err) => console.log(err));
